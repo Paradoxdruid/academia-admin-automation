7769a95c80ff2a9d2fa1ffedd53122c0f58483ae
Paradoxdruid <bonham@gmail.com>
2020-01-16 12:04:21 -0700

    Update process_enrollment.py

--- process_enrollment.py
+++ process_enrollment.py
@@ -8,6 +8,7 @@ Then, run this script on the downloaded text file.
 
 # Imports
 import csv
+import xlsxwriter
 from itertools import cycle
 import argparse
 
@@ -113,7 +114,7 @@ def main(filename, output_name, dept):
                 continue
 
             # remove leading and trailing whitespace
-            [(i, j.strip()) for (i, j) in enumerate(newlist)]
+            newlist = [i.strip() for i in newlist]
 
             # Add the entry to our output list
             newfile.append(newlist)
@@ -121,12 +122,114 @@ def main(filename, output_name, dept):
     # Remove final non-data lines
     newfile = newfile[:-2]
 
-    # Write the output
-    with open(output_name, "w", newline="") as writefile:
-        writer = csv.writer(writefile, delimiter=",")
-        for row in newfile:
-            writer.writerow(row)
+    # Send it to the output function
+    write_and_format(newfile, output_name)
 
+    # Finish
+    return
+
+
+def write_and_format(input_list, output_name):
+    """Take in a list of lists for output data and write an xlsx file.
+
+    Args:
+        input_list:
+            input data (a list of lists) of output to write.
+        output_name:
+            a filename to write out to.
+
+    Returns:
+        Nothing.
+    """
+
+    # Initialize the xlsx file
+    workbook = xlsxwriter.Workbook(output_name, {"strings_to_numbers": True})
+    worksheet = workbook.add_worksheet()
+
+    bold = workbook.add_format({"bold": True})
+
+    # Process the data
+    rowCount = 0
+    for row in input_list:
+        if rowCount == 0 or rowCount == 1:
+            colCount = 0
+            for column in row:
+                worksheet.write(rowCount, colCount, column, bold)
+                colCount += 1
+        else:
+            colCount = 0
+            for column in row:
+                worksheet.write(rowCount, colCount, column)
+                colCount += 1
+        rowCount += 1
+
+    # Set up for easy scrolling
+    worksheet.freeze_panes(2, 0)
+
+    # Format column widths
+    worksheet.set_column("A:A", 6.5)
+    worksheet.set_column("B:B", 7)
+    worksheet.set_column("C:C", 5.5)
+    worksheet.set_column("D:D", 6.5)
+    worksheet.set_column("E:E", 2)
+    worksheet.set_column("F:F", 6.5)
+    worksheet.set_column("G:G", 2)
+    worksheet.set_column("H:H", 13.2)
+    worksheet.set_column("I:I", 5.5)
+    worksheet.set_column("J:J", 4)
+    worksheet.set_column("K:K", 7)
+    worksheet.set_column("L:L", 5)
+    worksheet.set_column("M:M", 5)
+    worksheet.set_column("N:N", 5.5)
+    worksheet.set_column("O:O", 12)
+    worksheet.set_column("P:P", 7)
+    worksheet.set_column("Q:Q", 4)
+    worksheet.set_column("R:R", 3.5)
+    worksheet.set_column("S:S", 10.5)
+    worksheet.set_column("T:T", 14)
+
+    # Common cell formatting
+    # Light red fill with dark red text
+    format1 = workbook.add_format({"bg_color": "#FFC7CE", "font_color": "#9C0006"})
+    # Light yellow fill with dark yellow text
+    format2 = workbook.add_format({"bg_color": "#FFEB9C", "font_color": "#9C6500"})
+    # Green fill with dark green text.
+    format3 = workbook.add_format({"bg_color": "#C6EFCE", "font_color": "#006100"})
+    # Darker green fill with black text.
+    format4 = workbook.add_format({"bg_color": "#008000", "font_color": "#000000"})
+
+    # Add enrollment evaluation conditions
+    worksheet.conditional_format(
+        2,  # row 3
+        10,  # column K
+        rowCount - 1,  # last row
+        10,  # column K
+        {"type": "formula", "criteria": "=$K3>0.94*$J3", "format": format4},
+    )
+    worksheet.conditional_format(
+        2,  # row 3
+        10,  # column K
+        rowCount - 1,  # last row
+        10,  # column K
+        {"type": "formula", "criteria": "=$K3>0.8*$J3", "format": format3},
+    )
+    worksheet.conditional_format(
+        2,  # row 3
+        10,  # column K
+        rowCount - 1,  # last row
+        10,  # column K
+        {"type": "formula", "criteria": "=$K3<10", "format": format1},
+    )
+    worksheet.conditional_format(
+        2,  # row 3
+        12,  # column M
+        rowCount - 1,  # last row
+        12,  # column M
+        {"type": "cell", "criteria": ">", "value": 0, "format": format2},
+    )
+
+    # Close it out
+    workbook.close()
     return
 
 
@@ -142,8 +245,8 @@ if __name__ == "__main__":
 
     # Run the program, defaulting to Chemistry for convenience
     if not args.dept:
-        out = args.input[:-3] + "csv"
+        out = args.input[:-3] + "xslx"
         main(args.input, out, "CHE")
     else:
-        out = args.input[:-3] + "csv"
+        out = args.input[:-3] + "xlsx"
         main(args.input, out, args.dept)
